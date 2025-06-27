00bb82154e14e5e3a38beba5c2cd18ae451b1f1f
Rajeevkavala <68290177+Rajeevkavala@users.noreply.github.com>
2025-06-27 15:26:29 +0530

    Day-11

---
+++ Day-11/script.js
@@ -0,0 +1,74 @@
+// //How to print messages to the console in JavaScript
+// // This is a comment in JavaScript
+// console.log("Hello World");
+// console.warn("Warning: This is a warning message");
+// console.error("Error: This is an error message");
+
+// myFunction = () =>{
+//     console.log("Button clicked!");
+//     alert("Button clicked!");
+// }
+
+// ConfirmBtn = () => {
+//     var a = confirm("Are you sure you want to proceed?")
+//     console.log(a);
+//     if(a) {
+//         console.log("User clicked OK");
+//     } else {
+//         console.log("User clicked Cancel");
+//     }
+// }
+
+// promptBtn = () => {
+//     var name = prompt("Please enter your name:");    
+//     if(name) {
+//         console.log("Hello " + name);
+//     } else {
+//         console.log("No name entered");
+//     }
+// }
+
+// console.log(x);
+
+// var x =10;
+
+// var x;
+
+// function myFunction() {
+//     let x = 20; 
+//     console.log("Inside myFunction, x = " + x); // This will log 20
+// }
+
+// myFunction();
+// console.log("Outside myFunction, x = " + x); // This will log 10
+
+
+// //Variable declaration and assignment
+// let name = "Rajeev"; // String
+// let age = 20; // Number
+// let isStudent = true; // Boolean
+// let hobbies = ["reading", "gaming", "coding"]; // Array
+// let person = { // Object
+//     firstName: "Rajeev",
+//     lastName: "Kavala",
+//     age: 20
+// }; // Object
+// let nothing = null; // Null
+// let notDefined; // Undefined
+
+// // console.log(3%15);
+
+// console.log(typeof name); // String
+// console.log(typeof age); // Number
+// console.log(typeof isStudent); // Boolean
+// console.log(typeof hobbies); // Object (Arrays are objects in JavaScript)
+// console.log(typeof person); // Object
+// console.log(typeof nothing); // Object (null is considered an object in JavaScript)
+// console.log(typeof notDefined); // Undefined
+// console.log("Hello, " + name + "! You are " + age + " years old.");
+
+
+// function ShowDateTime() {
+//     var date = document.getElementById('demo').innerHTML = Date();
+//     alert("Current Date and Time: " + date);
+// }
